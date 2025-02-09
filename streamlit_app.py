d8434dbeb1cc2e3346b1d5c5eba9085e7a63339b
NPVASSISTANT <npvgroup.it@gmail.com>
2025-02-09 13:20:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -93,7 +93,7 @@ if prompt := st.chat_input("Bạn nhập nội dung cần trao đổi ở đây 
     with st.chat_message("assistant"):
         response = st.write_stream(stream)
     st.session_state.messages.append({"role": "assistant", "content": response})
-" Thím rất vui và phấn khởi được hỗ trợ Anh/Chị - Anh chị hỏi càng nhiều thím được sếp khen nhiều  "
+" Thím rất vui và phấn khởi được hỗ trợ Anh/Chị - Thím sẽ cố gắng học hỏi mỗi ngày để đuọc hoàn thiện hơn  "
 
 
 #####
